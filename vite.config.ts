7f77def5789e96a707857c6acaeac5c9a9da789c
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-08-04 17:36:16 -0400

    Actualizacion minima de configuracion vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/Web_Portfolio/', // Cambia esto por el nombre de tu repo
+	base: '/Web_Portfolio/',
 });
