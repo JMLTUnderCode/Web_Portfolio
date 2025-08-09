c798103f5d9b1f33a7ef0c63a363e5c30297e527
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-08-08 23:52:57 -0400

    Se instalan nuevas dependencias asociadas a tailwind y derivados. Se agregan configuraciones adicionales y creacion de archivo de configuracion para tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		tailwindcss(),
+		react()
+	],
 	base: '/Web_Portfolio/',
 });
