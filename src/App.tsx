d7b0bc019dd909332d98c8166c7742ef57c23a4e
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-13 14:26:11 -0400

    Modificacion de importacion y mejora de identacion

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,30 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import About from './pages/About';
-import Resume from './pages/Resume';
-import GitHub_Metrics from './pages/GitHub_Metrics';
+import { Resume } from './pages/Resume';
+import { GitHub_Metrics } from './pages/GitHub_Metrics';
 import Projects from './pages/Projects';
 import ScrollToTop from './components/animation/ScrollToTop';
 
 function App() {
-  	return (
-		<HashRouter>
-			<ScrollToTop />
-			<main>
-				<Header />
-				<div className="main-content">
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Navigate to="/about" replace />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/resume" element={<Resume />} />
-						<Route path="/github_metrics" element={<GitHub_Metrics />} />
-						<Route path="/projects" element={<Projects />} />
-					</Routes>
-				</div>
-			</main>
-		</HashRouter>
-	);
+    return (
+    <HashRouter>
+      <ScrollToTop />
+      <main>
+        <Header />
+        <div className="main-content">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/about" replace />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/github_metrics" element={<GitHub_Metrics />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </div>
+      </main>
+    </HashRouter>
+  );
 }
 
 export default App;
