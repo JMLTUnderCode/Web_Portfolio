bedcee64f2afff3df1753701615d98434b95ef76
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-16 15:41:36 -0400

    Inclusion y edicion de nuevas categorias de proyectos

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,6 @@ export const BASE_URL = import.meta.env.BASE_URL;
 export const CATEGORIES = {
   ALL: 'all',
   WEB_DEVELOPMENT: 'web development',
+  YOUTUBE_COURSE: 'youtube course',
+  UDEMY_COURSE: 'udemy course',
 }
